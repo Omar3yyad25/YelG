289ae7ccb5ab2438a57291ab7de3e1785f7a6da5
Omar W. Ayyad <202000806@zewailcity.edu.eg>
2024-11-09 15:09:08 +0200

    fixing build

--- src/redux/rootReducer.tsx
+++ src/redux/rootReducer.tsx
@@ -1,10 +1,6 @@
 // rootReducer.ts
 import { combineReducers } from "@reduxjs/toolkit";
 import videoModalReducer from "./features/videoModalSlice";
-import shopReducer from "./features/shopSlice";
-import shopSidebarReducer from "./features/shopSidebarSlice";
-import cartReducer from "./features/cartSlice";
-import wishlistReducer from "./features/wishlistSlice";
 import cartModalReducer from "./features/cartModalSlice";
 import searchModalReducer from "./features/searchModalSlice";
 import lightboxReducer from "./features/lightboxSlice";
@@ -12,10 +8,6 @@ import wishlistModalReducer from "./features/wishlistModalSlice";
 
 const rootReducer = combineReducers({
   videoModal: videoModalReducer,
-  shop: shopReducer,
-  shopSidebar: shopSidebarReducer,
-  cart: cartReducer,
-  wishlist: wishlistReducer,
   cartModal: cartModalReducer,
   searchModal: searchModalReducer,
   lightbox: lightboxReducer,
